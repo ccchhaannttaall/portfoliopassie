469837732027b0d75a69efe13c958a0d1871bedb
ccchhaannttaall <chantalvalk96@live.nl>
2020-06-03 19:18:09 +0200

    Pagina's toegevoegd aan het project, en hierin de html content geplaatst. nog te doen = css opsplitsen en routing van de pagina's.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProjectenComponent } from './projecten/projecten.component';
+import { ResultatenComponent } from './resultaten/resultaten.component';
+import { OverMijComponent } from './over-mij/over-mij.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectenComponent,
+    ResultatenComponent,
+    OverMijComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
